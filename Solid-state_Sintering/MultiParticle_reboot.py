96fd46317bd7b9c0a373673c27eed2b95d4b5c3a
bo-qian <bqian@shu.edu.cn>
2024-08-22 13:58:55 +0800

    名称调整

--- Solid-state_Sintering/MultiParticle_reboot.py
+++ Solid-state_Sintering/MultiParticle_reboot.py
@@ -393,7 +393,7 @@ def plot_function_on_mesh(fenics_function, mesh, time_current, title):
 #############################################################################################################
 
 # Read the input file ****************************************************************************************
-parameters_input = read_input_file("input_polyparticle_reboot.txt")
+parameters_input = read_input_file("input_MultiParticle_reboot.txt")
 # Phase-field parameters
 a = float(parameters_input.get("Alpha"))
 b = float(parameters_input.get("Beta"))
@@ -496,7 +496,7 @@ denim_reg = lambda x: x if abs(x) > 1e-12 else 1e-12
 print("the code being executed:", sys.argv)
 python_script_name = os.path.basename(sys.argv[0])
 shutil.copy(python_script_name, os.path.join(Codes_directory, python_script_name))
-shutil.copy("input_polyparticle_reboot.txt", os.path.join(Input_directory, "input_polyparticle_reboot.txt"))
+shutil.copy("input_MultiParticle_reboot.txt", os.path.join(Input_directory, "input_MultiParticle_reboot.txt"))
 
 # Input parameter processing **********************************************************************************
 # Calculate the total number of particles and diameter of particles
